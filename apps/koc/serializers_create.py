538c1a578fd09c0d3ae87ed768bb9446dda6eb04
jahidul hasan <hasanrabbie@gmail.com>
2026-09-18 20:06:27 +0600

    feat: add SubmissionFilterSerializer and refactor AdminSubmissionViewSet to use it for filtering submissions

--- apps/koc/serializers_create.py
+++ apps/koc/serializers_create.py
@@ -11,3 +11,10 @@ class SubmissionCreateSerializer(serializers.Serializer):
     )
     platform = serializers.ChoiceField(choices=PROVIDER_CHOICES, required=True)
     published_url = serializers.URLField(required=True, max_length=500)
+
+
+class SubmissionFilterSerializer(serializers.Serializer):
+    member_uuid = serializers.UUIDField(required=False)
+    from_date = serializers.DateField(required=False)
+    to_date = serializers.DateField(required=False)
+    platform = serializers.ChoiceField(choices=PROVIDER_CHOICES, required=False)
